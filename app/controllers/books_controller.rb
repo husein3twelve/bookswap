54b269bf7c6a1733d734b1d5e108cab5df266f87
Husein Kareem <husein3twelve@gmail.com>
2023-05-14 10:51:40 +0200

    add show book path

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,11 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 	end
+
+	def show
+		@book = Book.find(params[:id])
+	# error handling when id not found
+	rescue ActiveRecord::RecordNotFound
+		redirect_to root_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/books/:id", to: "books#show", as: :book
+
   # Defines the root path route ("/")
   root "books#index"
 end
