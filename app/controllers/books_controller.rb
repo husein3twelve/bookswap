29bfbd01b19ebaefdb892ba3e214e02f2b4d5f75
Husein Kareem <husein3twelve@gmail.com>
2023-05-15 10:37:17 +0200

    add function to create new book with form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,30 @@ class BooksController < ApplicationController
 
 	def show
 		@book = Book.find(params[:id])
-	# error handling when id not found
-	rescue ActiveRecord::RecordNotFound
-		redirect_to root_path
+		# error handling when id not found
+		rescue ActiveRecord::RecordNotFound
+			redirect_to root_path
+	end
+
+	def new
+		# creates new blog post in memory
+		@book = Book.new
+	end
+
+	def create
+		# saves to database
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to @book
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	# Book.new(params[:book]), instead due to security we have to build private method below and permit
+	private
+	def book_params
+		params.require(:book).permit(:title, :author)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/books/new", to: "books#new", as: :new_book
   get "/books/:id", to: "books#show", as: :book
+  post "books/post", to: "books#create", as: :books
 
   # Defines the root path route ("/")
   root "books#index"
