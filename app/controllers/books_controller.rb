3341cd8adfd533a77eda1b0b6fd520c255b668c3
Husein Kareem <husein3twelve@gmail.com>
2023-05-17 15:40:15 +0200

    setup devise authentication and added some navigation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-	# use except: instead of only: for inverse
+	
+	before_action :authenticate_user!, except: [:index, :show]
 	before_action :set_book, only: [:show, :edit, :update, :destroy]
 
 	def index
